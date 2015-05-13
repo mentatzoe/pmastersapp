6b3dcdfefaa7fc2d7db826318e2cbfe81822093e
Maria Lopez-Latorre <mentatzoe@gmail.com>
2015-05-13 15:51:31 +0200

    Basic routing
    Kinda sucks

--- com.pmasters.moodle/www/js/app/core/routing.js
+++ com.pmasters.moodle/www/js/app/core/routing.js
@@ -1,12 +1,22 @@
 var pmastersApp = angular.module('pmastersApp', ['ui.router']);
 pmastersApp.config(
     function($stateProvider, $urlRouterProvider){
-        $urlRouterProvider.otherwise('/home');
+        $urlRouterProvider.otherwise('/login');
 
         $stateProvider
-            .state(
-
-        );
+            .state('base', {
+                abstract:true,
+                url: '/',
+                templateUrl: 'index.html'
+            })
+            .state('base.login',{
+                url: '/login',
+                views: {
+                    'content@base': {
+                        templateUrl: "test1.html"
+                    }
+                }
+            })
 
     }
-)
+);
