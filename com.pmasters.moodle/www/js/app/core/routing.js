24a89ca58fdf3a799bf216123a7a3d9f67569b24
Maria Lopez-Latorre <mentatzoe@gmail.com>
2015-05-13 17:05:42 +0200

    Routing fix
    Slight route polish

--- com.pmasters.moodle/www/js/app/core/routing.js
+++ com.pmasters.moodle/www/js/app/core/routing.js
@@ -1,20 +1,37 @@
 var pmastersApp = angular.module('pmastersApp', ['ui.router']);
+console.log("What");
 pmastersApp.config(
     function($stateProvider, $urlRouterProvider){
         $urlRouterProvider.otherwise('/login');
 
         $stateProvider
+            .state('login', {
+                url:'/login',
+                templateUrl: 'login.html'
+            })
             .state('base', {
-                abstract:true,
-                url: '/',
+                url:'/home',
                 templateUrl: 'index.html'
             })
-            .state('base.login',{
-                url: '/login',
-                views: {
-                    'content@base': {
-                        templateUrl: "test1.html"
-                    }
+            .state('base.calendar',{
+                url: '^/calendar',
+                template: "calendar",
+                controller: function() {
+                    console.log("HELLO");
+                }
+            })
+            .state('base.messages',{
+                url: '^/messages',
+                template: "messages",
+                controller: function() {
+                    console.log("HELLO");
+                }
+            })
+            .state('base.pref',{
+                url: '^/preferences',
+                template: "prefs",
+                controller: function() {
+                    console.log("HELLO");
                 }
             })
 
